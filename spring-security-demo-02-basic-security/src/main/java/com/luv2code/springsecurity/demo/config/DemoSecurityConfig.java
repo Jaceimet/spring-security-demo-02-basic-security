abba9f80028b129cd1cbbee40d4e49d3f3b4417c
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-02-13 12:51:25 -0800

    Error on demoSecurityConfig
    Need to review code

--- spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,30 +1,57 @@
 package com.luv2code.springsecurity.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+public class DemoSecurityConfig {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+    	
+        UserDetails john = User.builder()
+            .username("john")
+            .password("{noop}test123")
+            .roles("EMPLOYEE")
+            .build();
 
-		// add our users for in memory authentication
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("MANAGER")
+                .build();
 
-		UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("ADMIN")
+                .build();
+        
+        return new InMemoryUserDetailsManager(john, mary, susan);  
+    }
 
-		auth.inMemoryAuthentication()
-			.withUser(User.withUsername("john").password("test123").roles("EMPLOYEE"))
-			.withUser(User.withUsername("mary").password("test123").roles("MANAGER"))
-			.withUser(User.withUsername("susan").password("test123").roles("ADMIN"));
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    		
+	    return http
+		.authorizeRequests(configurer ->
+			configurer
+				.anyRequest()
+				.authenticated())
+		.formLogin(configurer ->
+			configurer
+				.loginPage("/showMyLoginPage")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll())
+		
+		.build();  
 
-	}
-
-
-
+    }	
 }
